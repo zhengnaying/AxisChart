db259fe935b420ae07fc9cbed148d4f3afab196d
zhengnaying <55484284+zhengnaying@users.noreply.github.com>
2020-11-14 10:37:29 +0800

    1

--- app/src/main/java/com/example/axischart/MainActivity.java
+++ app/src/main/java/com/example/axischart/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.axischart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView txtFunction=findViewById(R.id.txtFunction);
+        final CustomView customView=findViewById(R.id.plotview);
+
+        Button buttonPlot=findViewById(R.id.buttonPlot);
+        buttonPlot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customView.NoZoom();
+                if(customView!=null){
+                    String strFunction=txtFunction.getText().toString();
+                    customView.setStrFunction(strFunction);
+                    customView.invalidate();
+                }
+            }
+        });
+    }
     }
 }
